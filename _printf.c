f4d3b2a17c8591be26204be9412dedbdebb88b31
rolit3 <hledrurolin@gail.com>
2023-01-21 17:54:39 -0500

    Update latest

--- _printf.c
+++ _printf.c
@@ -4,7 +4,7 @@ void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
- * @format: format.
+ * @format: format
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
@@ -26,7 +26,6 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
